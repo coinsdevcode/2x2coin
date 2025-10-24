bba79a9d0874b6b2da1a5d374c37e7dd46690fcf
coindev <webmastercase20@gmail.com>
2025-10-24 11:20:20 -0300

    v1.3.1.2 update

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -126,7 +126,7 @@ extern const CRPCTable tableRPC;
 
 extern int64_t nWalletUnlockTime;
 extern int64_t AmountFromValue(const json_spirit::Value& value);
-extern json_spirit::Value ValueFromAmount(int64_t amount);
+extern json_spirit::Value ValueFromAmount(__int128 amount);
 extern json_spirit::Value ValueFromBigNumAmount(CBigNum amount);
 extern double GetDifficulty(const CBlockIndex* blockindex = NULL);
 
